e3326ecf19a2b1eed5fec9bac4adbe03c3ceb6cc
bizip <bizip04@gmail.com>
2022-09-10 22:24:23 +0200

    Add categoriy model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
+  resources :categories, only: [:index, :show, :new, :create, :destroy]
+
 end
