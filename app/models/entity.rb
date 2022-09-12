1e621e2cb8e0b5b10ab34cc712238bbaa7524515
bizip <bizip04@gmail.com>
2022-09-12 08:15:30 +0200

    Add all relationships

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,5 @@
 class Entity < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
+    has_many :entity_group
+    has_many :entities, through: :entity_group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
+    has_many :entity_group
+    has_many :groups, through: :entity_group
 end
