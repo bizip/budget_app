b6c28c54036ee8ff0f813c9459e571efcdaae6a9
bizip <bizip04@gmail.com>
2022-09-12 15:52:04 +0200

    Add new add category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,20 @@
-# frozen_string_literal: true
 
-class CategoriesController < ApplicationController # rubocop:todo Style/Documentation
+class CategoriesController < ApplicationController
   def index; end
+
+  def new
+    @category = Group.new
+  end
+
+  def create
+    @category = Group.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "#{@category.name} successfully created."
+    end
+  end
+
+  private
+  def category_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
