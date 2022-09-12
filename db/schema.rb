7ab273fe77f4fae7edb3869792122b9b610d5cf7
bizip <bizip04@gmail.com>
2022-09-12 08:26:28 +0200

    Add add models and all posible relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_060838) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_061807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,11 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_060838) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "entity_groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "entity_id"
+    t.bigint "group_id"
+    t.index ["entity_id"], name: "index_entity_groups_on_entity_id"
+    t.index ["group_id"], name: "index_entity_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -31,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_060838) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_060838) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "users", column: "author_id"
+  add_foreign_key "entity_groups", "entities"
+  add_foreign_key "entity_groups", "groups"
+  add_foreign_key "groups", "users", column: "author_id"
 end
