1ef4be7d231dc812e9c33163487f19ddd240b58a
bizip <bizip04@gmail.com>
2022-09-13 11:10:08 +0200

    Fix the controller linter errors issue

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,25 +1,29 @@
-class EntitiesController <  ApplicationController
-    def index
-        @entity = Entity.all
-    end
+# frozen_string_literal: true
 
-    def new
-        @entity = Entity.new
-    end
+class EntitiesController < ApplicationController
+  def index
+    @entity = Entity.all
+  end
 
-    def create
-        @entity = Entity.new(entity_params)
-        @entity.author = current_user
-        if @entity.save
-            redirect_to entities_path, notice: "#{@entity.name} successfully created." if @entity.save
-            flash[:notice] = 'Transaction created successfully!'
-          else
-            render :new
-            flash.now[:alert] = 'Something unexpected happened, Transaction could not be created!'
-          end
+  def new
+    @entity = Entity.new
+  end
+
+  def create
+    @entity = Entity.new(entity_params)
+    @entity.author = current_user
+    if @entity.save
+      redirect_to entities_path, notice: "#{@entity.name} successfully created." if @entity.save
+      flash[:notice] = 'Transaction created successfully!'
+    else
+      render :new
+      flash.now[:alert] = 'Something unexpected happened, Transaction could not be created!'
     end
-    private
-    def entity_params
-        params.require(:entity).permit(:name, :amount)
-    end
+  end
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 end
