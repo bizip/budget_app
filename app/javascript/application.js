a9ced5d6043651de4993b85af5aa1848b4c861b5
bizip <bizip04@gmail.com>
2022-09-15 15:45:25 +0200

    Add a popup section for navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@fortawesome/fontawesome-free"
+
+let divbtn = document.getElementById("nav_btn");
+divbtn.addEventListener("click", () => {
+    let popup = document.getElementById('popup');
+    if (popup.style.display === "none"){
+        popup.style.display="flex"
+    }else{
+        popup.style.display="none"
+
+    }
+})
+
+let popup = document.getElementById('popup');
+popup.addEventListener("click", ()=>{
+    popup.style.display="none"
+})
+
