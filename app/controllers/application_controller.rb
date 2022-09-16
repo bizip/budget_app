12827d9655ecd62da02d5d2ddc5ea4676996a4c7
bizip <bizip04@gmail.com>
2022-09-16 08:22:10 +0200

    Add full name section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :conifgure_parameters, if: :devise_controller?
+
+  def conifgure_parameters
+  attributes = [:name, :email, :password]
+  devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
 end
